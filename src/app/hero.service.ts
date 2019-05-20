247df0b4ecce7faf26152c2a56e2e7a520c381cd
aymomeni <aymomeni@gmail.com>
2019-05-20 16:53:41 -0600

    adding http request/response functionality in hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,19 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
@@ -18,6 +24,12 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]>{
     this.messageService.add("HeroService: fetched Heroes");
-    return of(HEROES);
+    //return of(HEROES); previously
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
